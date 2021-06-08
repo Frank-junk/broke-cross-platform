35a6c73644cdc26b4945139e8f184a433dfad5ca
aimeiyijia <1456723200@qq.com>
2021-06-08 15:01:11 +0800

    调整人脸识别页面

--- pages/faceVerify/index.vue
+++ pages/faceVerify/index.vue
@@ -1,17 +1,29 @@
 <template>
   <view class="face-page">
     <image class="face-img" src="/static/images/face_verify.png"></image>
-    <u-form :model="faceForm" ref="faceFormRule" class="face-form">
-      <u-form-item left-icon="account" prop="realName" class="realName-item">
-        <u-input v-model="faceForm.realName" placeholder="请输入真实姓名" />
-      </u-form-item>
-      <u-form-item left-icon="account" prop="idCard" class="idCard-item">
-        <u-input v-model="faceForm.idCard" placeholder="请输入证件号码" />
-      </u-form-item>
-    </u-form>
-    <u-button :ripple="true" :hair-line="false" class="face-submit">
-      实名认证
-    </u-button>
+    <view class="face">
+      <u-form :model="faceForm" ref="faceFormRule" class="face-form">
+        <u-form-item left-icon="account" prop="realName" class="realName-item">
+          <u-input v-model="faceForm.realName" placeholder="请输入真实姓名" />
+        </u-form-item>
+        <u-form-item left-icon="account" prop="idCard" class="idCard-item">
+          <u-input v-model="faceForm.idCard" placeholder="请输入证件号码" />
+        </u-form-item>
+      </u-form>
+      <u-radio-group v-model="faceForm.radio" class="face-type">
+        <u-radio
+          v-for="(item, index) in radioList"
+          :key="index"
+          :name="item.name"
+          :disabled="item.disabled"
+        >
+          {{ item.name }}
+        </u-radio>
+      </u-radio-group>
+      <u-button :ripple="true" :hair-line="false" class="face-submit">
+        实名认证
+      </u-button>
+    </view>
   </view>
 </template>
 
@@ -24,7 +36,19 @@
           againPassword: '',
           realName: '',
           idCard: '',
+          radio: '反光识别',
         },
+
+        radioList: [
+          {
+            name: '反光识别',
+            disabled: false,
+          },
+          {
+            name: '读数识别',
+            disabled: false,
+          },
+        ],
         faceFormRules: {
           realName: [
             {
@@ -59,11 +83,8 @@
       height: 380rpx;
       margin-bottom: 24px;
     }
-    .face-form {
+    .face {
       padding: 0 24px;
-      /deep/ .u-form {
-        padding: 0 24px;
-      }
     }
     /deep/ .u-form-item {
       border: 1px solid #e4e7ed;
@@ -78,12 +99,16 @@
     /deep/ .idCard-item .u-form-item {
       border-top: none;
     }
+    .face-type {
+      display: inline-block;
+      margin-top: 24px;
+    }
     .face-submit {
       margin-top: 24px;
       background-color: #0079fe;
       color: #fff;
       border-radius: 8rpx;
-      width: calc(100% - 48px);
+      width: 100%;
       /deep/ button {
         background-color: transparent;
         color: #fff;
